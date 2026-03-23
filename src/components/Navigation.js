3c48cfab0d4fa627b04795de4c9635054bfa9e39
Deependra Patel <satyampatelbankhedi@gmail.com>
2026-03-23 18:53:32 +0530

    Update logo size in Navigation component for improved visibility

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -147,7 +147,7 @@ export default function Navigation() {
           <img
             src="/intellosoft-logoooo.png"
             alt="Intellosoft logo"
-            className="h-12 w-auto object-contain"
+            className="h-16 w-auto object-contain"
           />
         </Link>
 
